1ee0bcb51ed14bf0986de52476a9d0d57254f72b
donaghue <donaghue@ncmath.net>
2019-01-23 18:08:13 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 
 class Environment:
@@ -88,29 +89,19 @@ class AgentProgram_reflex:
         #3 ', wall, clean
         if sensor_status%2 == 0:
             self.agent.clean()
-            print('cleaning')
         elif sensor_status%4 == 1:
             self.agent.move_forward()
-            print('moving')
         elif sensor_status == 3:
             self.agent.rotate_right()
-            print('right')
         elif sensor_status ==7:
             self.turnoff()
-            print('off')
-
-class AgentProgram_stocastic:
+class AgentProgram_reflex_mem:
     def __init__(self, agent):
         self.agent = agent
         self._on = 1
-
-        # a_0 = prob clean
-        # a_1 = prob right
-        # a_2 = prob left
-        # a_3 = prob forward
-        # a_4 = prob off
-
-        status_0_prob = ()
+        self._actions=[self.agent.clean, self.agent.rotate_right, self.agent.rotate_left, self.agent.move_forward,
+                       self.turnoff]
+        self.memory = 0
 
     def turnoff(self):
         self._on = 0
@@ -119,24 +110,180 @@ class AgentProgram_stocastic:
         return self._on == 1
 
     def proceed(self):
-        sensor_status = self.agent.sensor_home()*2**2 + self.agent.sensor_wall()*2 + self.agent.sensor_clean()
+        if self.agent.sensor_clean() == 0:
+            self.agent.clean()
+
+        elif self.agent.sensor_clean() + self.agent.sensor_wall() + self.agent.sensor_home() ==3:
+            self.turnoff()
+
+        elif self.agent.sensor_wall() == 1:
+            if self.memory == 0:
+                self.agent.rotate_right()
+                self.memory = 1
+            elif self.memory ==2:
+                self.agent.rotate_right()
+                self.memory = 3
+            elif self.memory == 3:
+                self.agent.rotate_left()
+                self.memory = 4
+            elif self.memory == 4:
+                self.agent.rotate_left()
+                self.memory = 5
+            elif self.memory ==5:
+                self.agent.rotate_left()
+                self.memory = 3
+
+        elif self.agent.sensor_clean() ==1:
+            if self.memory == 0:
+                self.agent.move_forward()
+
+            elif self.memory == 1:
+                self.agent.move_forward()
+                self.memory = 2
+            elif self.memory == 2:
+                self.agent.rotate_right()
+                self.memory = 3
+            elif self.memory == 3:
+                self.agent.move_forward()
+            elif self.memory == 4:
+                self.agent.move_forward()
+                self.memory = 5
+            elif self.memory == 5:
+                self.agent.rotate_left()
+                self.memory = 0
+class AgentProgram_stocastic:
+    def __init__(self, agent):
+        self.agent = agent
+        self._on = 1
+        self._actions=[self.agent.clean, self.agent.rotate_right, self.agent.rotate_left, self.agent.move_forward,
+                       self.turnoff]
+        self._action_probabilities = np.array([[1, 0, 0, 0, 0],  #dirty
+                                [0, 0.138, 0.088, 1 - 0.138 - 0.088, 0],  #clean
+                                [1, 0, 0, 0, 0],  # dirty, wall
+                                [0, 1, 0, 0, 0],  #clean, wall
+                                [1, 0, 0, 0, 0],  # dirty, home
+                                [0, 0, 0, 1, 0],  # clean, home
+                                [1, 0, 0, 0, 0],  # dirty, wall, home
+                                [0, 1, 0, 0, 0.0]]) #clean, wall, home
+
+    def turnoff(self):
+        self._on = 0
+
+    def on(self):
+        return self._on == 1
+
+    def proceed(self):
+        sensor_status = int(self.agent.sensor_home()*2**2 + self.agent.sensor_wall()*2 + self.agent.sensor_clean())
         #0, not home, no wall, not clean
         #1, ', ', clean,
         #2 ', wall, dirty,
         #3 ', wall, clean
-        if sensor_status%2 == 0:
-            self.agent.clean()
-            print('cleaning')
-        elif sensor_status%4 == 1:
-            self.agent.move_forward()
-            print('moving')
-        elif sensor_status == 3:
-            self.agent.rotate_right()
-            print('right')
-        elif sensor_status ==7:
-            self.turnoff()
-            print('off')
+        r =  np.random.random_sample()
+        self._actions[0]()
+        for j in range(0,4):
+            s = sum(self._action_probabilities[ sensor_status, 0:j+1])
+            if r <= s:
+                self._actions[j]()
+                return
 
+class AgentProgram_stocastic_learner:
+    def __init__(self, agent, alpha=0.15, beta=0.15, gamma=0.5):
+        self.agent = agent
+        self._on = 1
+        self._actions=[self.agent.clean, self.agent.rotate_right, self.agent.rotate_left, self.agent.move_forward,
+                       self.turnoff]
+        self._action_probabilities = np.array([[1, 0, 0, 0, 0],  #dirty
+                                [0, alpha, beta, 1 - alpha - beta, 0],  #clean
+                                [1, 0, 0, 0, 0],  # dirty, wall
+                                [0, gamma, 1 - gamma, 0, 0],  #clean, wall
+                                [1, 0, 0, 0, 0],  # dirty, home
+                                [0, 0, 0, 1, 0],  # clean, home
+                                [1, 0, 0, 0, 0],  # dirty, wall, home
+                                [0, 0.75, 0, 0, 0.25]]) #clean, wall, home
+
+    def turnoff(self):
+        self._on = 0
+
+    def on(self):
+        return self._on == 1
+
+    def proceed(self):
+        sensor_status = int(self.agent.sensor_home()*2**2 + self.agent.sensor_wall()*2 + self.agent.sensor_clean())
+        #0, not home, no wall, not clean
+        #1, ', ', clean,
+        #2 ', wall, dirty,
+        #3 ', wall, clean
+        r =  np.random.random_sample()
+        self._actions[0]()
+        for j in range(0,4):
+            s = sum(self._action_probabilities[ sensor_status, 0:j+1])
+            if r <= s:
+                self._actions[j]()
+                return
+
+def stocastic_trainer():
+    #define default parameters
+    # alpha = 0.138, beta = 0.08, avg ~ 720
+    best_parameters = np.array([0.1, 0.1, 0.5, 0])
+    avg_steps = 0
+    tr=500
+    for test in range(0, tr):
+        env_no_wall = Environment(0)
+        robot_no_wall = Agent(env_no_wall, (0, 0), (0, 0), 0)
+        AI_no_walls = AgentProgram_stocastic_learner(robot_no_wall, best_parameters[0], best_parameters[1], best_parameters[2])
+        env_wall = Environment(1)
+        robot_wall = Agent(env_wall, (0, 0), (0, 0), 0)
+        AI_walls = AgentProgram_stocastic_learner(robot_wall, best_parameters[0], best_parameters[1], best_parameters[2])
+        s = 0
+        t = 0
+        while np.sum(env_no_wall.floor) < 90:
+            s += 1
+            AI_no_walls.proceed()
+
+
+        while np.sum(env_wall.floor) < 90:
+            t += 1
+            AI_walls.proceed()
+
+        avg_steps += (s + t)
+    best_parameters[3] = avg_steps/(2*tr)
+    print(best_parameters)
+
+    #vary parameters and tests
+    for trial in range(0, 10):
+        print(trial)
+        best_outcome = best_parameters
+        for run in range(0, 10):
+            alpha = 0.05*(np.random.random_sample() - 0.5) + best_parameters[0]
+            beta = 0.05*(np.random.random_sample() - 0.5) +  best_parameters[1]
+            gamma= 0.05*(np.random.random_sample() -0.5) + best_parameters[2]
+            avg_steps = 0
+            for test in range(0, tr):
+                env_no_wall = Environment(0)
+                robot_no_wall = Agent(env_no_wall, (0, 0), (0, 0), 0)
+                AI_no_walls = AgentProgram_stocastic_learner(robot_no_wall, alpha, beta, gamma)
+                env_wall = Environment(1)
+                robot_wall = Agent(env_wall, (0, 0), (0, 0), 0)
+                AI_walls = AgentProgram_stocastic_learner(robot_wall, alpha, beta, gamma)
+                s = 0
+                t = 0
+                while np.sum(env_no_wall.floor) < 90:
+                    s += 1
+                    AI_no_walls.proceed()
+
+                while np.sum(env_wall.floor) < 90:
+                    t += 1
+                    AI_walls.proceed()
+
+                avg_steps += (s + t)
+            avg_steps = avg_steps/(2*tr)
+            if avg_steps < best_outcome[3]:
+                best_outcome[0] = alpha
+                best_outcome[1] = beta
+                best_outcome[2] = gamma
+                best_outcome[3] = avg_steps
+        best_parameters = best_outcome
+        print(best_parameters)
 
 def render(env, agent, history= []):
     plt.axis('off')
@@ -170,12 +317,80 @@ def render(env, agent, history= []):
         ax.add_artist(plt.Circle((agent.position[0] + 0.4, agent.position[1] + 0.5), 0.1, color='red'))
     plt.show()
 
-env = Environment(0)
-robot = Agent(env, (0,0), (0,0), 0)
-AI = AgentProgram_reflex(robot)
-history_x = []
-history_y = []
-while AI.on():
-    AI.proceed()
+def animate(env, AI, intv=50):
+    fig, (ax, ax2) = plt.subplots(1 ,2)
+    fig.set_dpi(100)
+    fig.set_size_inches(7, 6.5)
 
-render(env, robot)
+    ax.set_xlim([0, 10])
+    ax.set_ylim([0,10])
+    ax.set_aspect('equal')
+    ax.grid(True, linestyle=':')
+
+    ax2.set_xlim([0, 500])
+    ax2.set_ylim([0,100])
+    #plt.axis('equal')
+    #plt.grid(True)
+
+    #line
+    linex = [AI.agent.position[0] + 0.5]
+    liney = [AI.agent.position[1] + 0.5]
+    line, = ax.plot(linex, liney, color='blue')
+
+    #clean
+    progy = [np.sum(env.floor)]
+    progx = [0]
+    prog, = ax2.plot(progx, progy, color='red')
+
+    #walls
+    for i in range(0, 10):
+        for j in range(0, 10):
+            if env.walls[i, j, 0] == 1:  # north
+                ax.plot([i, i + 1], [j + 1, j + 1], 'k')
+            if env.walls[i, j, 1] == 1:  # east
+                ax.plot([i + 1, i + 1], [j, j + 1], 'k')
+            if env.walls[i, j, 2] == 1:  # south
+                ax.plot([i, i + 1], [j, j], 'k')
+            if env.walls[i, j, 3] == 1:  # west
+                ax.plot([i, i], [j, j + 1], 'k')
+
+
+    #robot
+    robot = []
+    robot.append(plt.Circle((AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.5, ), 0.2, color='blue'))
+    if AI.agent.orientation == 0:
+        robot.append(plt.Circle((AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.6), 0.1, color='red'))
+    if AI.agent.orientation == 1:
+        robot.append(plt.Circle((AI.agent.position[0] + 0.6, AI.agent.position[1] + 0.5), 0.1, color='red'))
+    if AI.agent.orientation == 2:
+        robot.append(plt.Circle((AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.4), 0.1, color='red'))
+    if AI.agent.orientation == 3:
+        robot.append(plt.Circle((AI.agent.position[0] + 0.4, AI.agent.position[1] + 0.5), 0.1, color='red'))
+    ax.add_patch(robot[0])
+    ax.add_patch(robot[1])
+
+
+    def update(i):
+        AI.proceed()
+        robot[0].center = (AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.5,)
+        if AI.agent.orientation == 0:
+            robot[1].center = (AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.6)
+        if AI.agent.orientation == 1:
+            robot[1].center = (AI.agent.position[0] + 0.6, AI.agent.position[1] + 0.5)
+        if AI.agent.orientation == 2:
+            robot[1].center = (AI.agent.position[0] + 0.5, AI.agent.position[1] + 0.4)
+        if AI.agent.orientation == 3:
+            robot[1].center = (AI.agent.position[0] + 0.4, AI.agent.position[1] + 0.5)
+        linex.append(AI.agent.position[0] + 0.5)
+        liney.append(AI.agent.position[1] + 0.5)
+        line.set_xdata(linex)
+        line.set_ydata(liney)
+        progy.append(np.sum(env.floor))
+        progx.append(len(progy) - 1)
+        prog.set_xdata(progx)
+        prog.set_ydata(progy)
+        ax2.set_xlim([0, 100 + 100*np.floor(len(progy)/100)])
+        return robot[0], robot[1], line, prog
+
+    anim = animation.FuncAnimation(fig, update, interval=intv, blit=True)
+    plt.show()
